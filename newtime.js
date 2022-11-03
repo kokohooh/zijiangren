b6775294e43c48c4ad3b642bfc8ffdbdbecd121a
kokohooh <109791642+kokohooh@users.noreply.github.com>
2022-11-03 10:45:42 +0800

    Update newtime.js

--- newtime.js
+++ newtime.js
@@ -22,20 +22,20 @@ function load(){
         if (5<hour && hour<20) {
             
             if (5<hour && hour<9) {
-                document.getElementsByClassName('board-index')[0].style.background = "url('https://s2.loli.net/2022/10/29/LzVnSmBT1KlqOcs.jpg')no-repeat center center fixed "
+                document.getElementsByClassName('board-index')[0].style.background = "url('https://s2.loli.net/2022/11/03/XKoTQktRUMAYf4N.jpg')no-repeat center center fixed "
                 document.getElementsByClassName("board-index")[0].style.backgroundSize = "cover";
             }
             if (15<hour && hour<18) {
-                document.getElementsByClassName('board-index')[0].style.background = "url('https://s2.loli.net/2022/10/29/LzVnSmBT1KlqOcs.jpg')no-repeat center center fixed "
+                document.getElementsByClassName('board-index')[0].style.background = "url('https://s2.loli.net/2022/11/03/XKoTQktRUMAYf4N.jpg')no-repeat center center fixed "
                 document.getElementsByClassName("board-index")[0].style.backgroundSize = "cover";
             }
             if (17<hour && hour<20) {
-                document.getElementsByClassName('board-index')[0].style.background = "url('https://s2.loli.net/2022/10/29/LzVnSmBT1KlqOcs.jpg')no-repeat center center fixed "
+                document.getElementsByClassName('board-index')[0].style.background = "url('https://s2.loli.net/2022/11/03/XKoTQktRUMAYf4N.jpg')no-repeat center center fixed "
                 document.getElementsByClassName("board-index")[0].style.backgroundSize = "cover";
             }
         }
         else{
-            document.getElementsByClassName('board-index')[0].style.background = "url('https://s2.loli.net/2022/10/29/LzVnSmBT1KlqOcs.jpg')no-repeat center center fixed "
+            document.getElementsByClassName('board-index')[0].style.background = "url('https://s2.loli.net/2022/11/03/XKoTQktRUMAYf4N.jpg')no-repeat center center fixed "
             document.getElementsByClassName("board-index")[0].style.backgroundSize = "cover";
         }
 
