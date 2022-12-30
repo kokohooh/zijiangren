2e6228aab6f9bad36382f01a2d433e93a1ab9bd5
kokohooh <109791642+kokohooh@users.noreply.github.com>
2022-12-30 16:31:34 +0800

    Update zhuanhuan.js

--- mnq/zhuanhuan.js
+++ mnq/zhuanhuan.js
@@ -2,50 +2,65 @@ var xiaoqianqian =14400
 var Jochan =10
 document.getElementById("JO").innerHTML ="Jo缠之⚪："+ Jochan
 document.getElementById("qian").innerHTML ="小钱钱："+ xiaoqianqian
+
+function jo1(){
+    if(xiaoqianqian<160){
+        document.getElementById("win2").innerHTML = "钱不够"
+
+    }
+    else{
+        xiaoqianqian= xiaoqianqian-160
+            Jochan= Jochan + 1
+            document.getElementById("JO").innerHTML ="Jo缠之⚪："+ Jochan
+            document.getElementById("qian").innerHTML ="小钱钱："+ xiaoqianqian
+    }
+}
+
+function jo10(){
+    if(xiaoqianqian<1600){
+        document.getElementById("win2").innerHTML = "钱不够"
+
+    }
+    else{
+        xiaoqianqian= xiaoqianqian-1600
+            Jochan= Jochan + 10
+            document.getElementById("JO").innerHTML ="Jo缠之⚪："+ Jochan
+            document.getElementById("qian").innerHTML ="小钱钱："+ xiaoqianqian
+
+    }
+}
+
+function jo90(){
+    if(xiaoqianqian<14400){
+        document.getElementById("win2").innerHTML = "钱不够"
+
+    }
+    else{
+        xiaoqianqian= xiaoqianqian-14400
+            Jochan= Jochan + 90
+            document.getElementById("JO").innerHTML ="Jo缠之⚪："+ Jochan
+            document.getElementById("qian").innerHTML ="小钱钱："+ xiaoqianqian
+    }
+}
 function jochan(){
     if(document.getElementById("jozhankai").name == "1"){
         document.getElementById("wen1").innerHTML ="160:1，兑换"
-        document.getElementById("shuru").style.display="block"
-        document.getElementById("ke").style.display="block"
-        document.getElementById("queding").style.display="block"
+        document.getElementById("huan1").style.display="block"
+        document.getElementById("huan10").style.display="block"
+        document.getElementById("huan90").style.display="block"
         document.getElementById("jozhankai").innerHTML = "-"
         document.getElementById("jozhankai").name = "0"
     }
  else{if (document.getElementById("jozhankai").name == "0" ){
     document.getElementById("wen1").innerHTML =""
-    document.getElementById("shuru").style.display="none"
-    document.getElementById("ke").style.display="none"
-    document.getElementById("queding").style.display="none"
+    document.getElementById("huan1").style.display="none"
+    document.getElementById("huan10").style.display="none"
+    document.getElementById("huan90").style.display="none"
     document.getElementById("jozhankai").innerHTML = "+" 
     document.getElementById("jozhankai").name = "1"
 
-}}
+}}}
 
-
-
-
-
-}
-function queding(){
-    var shuliang =document.getElementById("shuru").value;
-    var shuliang160 =shuliang * 160
-    document.getElementById("JO").innerHTML ="Jo缠之⚪："+ Jochan
-document.getElementById("qian").innerHTML ="小钱钱："+ xiaoqianqian
-
-    
-if(xiaoqianqian<shuliang160){
-    document.getElementById("wen1").innerHTML ="钱不够"
-document.getElementById("qian").innerHTML ="小钱钱："+ xiaoqianqian
-
-}
-else{
-    xiaoqianqian = xiaoqianqian-shuliang160
-    Jochan=parseInt(Jochan)+parseInt(shuliang)
-document.getElementById("qian").innerHTML ="小钱钱："+ xiaoqianqian
-   
-    document.getElementById("JO").innerHTML ="Jo缠之⚪："+ Jochan
-}
-}
 function qianzhankai(){
     if(document.getElementById("qianzhankai").name == "1"){
         document.getElementById("yiju").style.display = "block"
