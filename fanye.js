f02438a5b11547c983dddeff9d2e66c28d3fd85f
kokohooh <109791642+kokohooh@users.noreply.github.com>
2023-01-09 12:32:41 +0800

    Add files via upload

---
+++ fanye.js
@@ -0,0 +1,8 @@
+function er(){
+    document.getElementById("yi").style.display="none"
+    document.getElementById("er").style.display="block"
+}
+function yi(){
+    document.getElementById("yi").style.display="block"
+    document.getElementById("er").style.display="none"
+}
